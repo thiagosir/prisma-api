eae372859b0ab79f38080886cb9656818ca75d9c
thiagosir <thiagobento9@hotmail.com>
2023-09-19 10:39:24 -0300

    Criando estrutura do CRUD de Users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+    id: number;
+    email: string;
+    name: string;
+    admin: boolean;
+    createdAt: Date;
+
+}
