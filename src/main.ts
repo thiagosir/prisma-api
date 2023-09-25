a89011d6fc9df7e41db9506b896de90e307c3ffc
thiagosir <thiagobento9@hotmail.com>
2023-09-25 10:07:33 -0300

    Documentacao da API no Swagger

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,24 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('The Test API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true
   }))
-  // app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new ConflictInterceptor())
   app.useGlobalInterceptors(new DatabaseInterceptor())
   app.useGlobalInterceptors(new UnauthorizedInterceptor())
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({ description: 'Email do Usuario' })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({ description: 'Nome completo do usuario' })
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty({ description: 'Define se o usuario é admin', default: false })
     @IsBoolean()
     admin: boolean;
 }
